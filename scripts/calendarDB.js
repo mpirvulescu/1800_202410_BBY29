5771973fcfaffcb5bb25dea87f0771ddbadd1329
Gurvir Dhami <gurvirsd@gmail.com>
2024-03-27 13:36:19 -0700

    minor changes to calendar

--- scripts/calendarDB.js
+++ scripts/calendarDB.js
@@ -1,44 +1,4 @@
-/*
-Calendar
-    Event00000001
-        date: March 20, 2024 at 6:00:00 PM UTC-7
-        event_id: 1
-        hour: 2
-        name: "COMP 1800"
-        uid: "ezsZTUIoe5cxKLSOCsLwOSgLg4N2"
-        user: "gurvirsd"
 
-users
-    user0
-        email: "gurvirsd@gmail.com"
-        name: "gurvirsd"
-        uid: "ezsZTUIoe5cxKLSOCsLwOSgLg4N2"
-
-        mysoccerteam@gmail.com
-        Mar 7, 2024
-        Mar 7, 2024
-        263o0IIx1JQMMiVkR05spa1r8833
-
-        gurvirsd@gmail.com
-        Mar 6, 2024
-        Mar 7, 2024
-        ezsZTUIoe5cxKLSOCsLwOSgLg4N2
-
-        alexhyeoksu@gmail.com
-        Mar 4, 2024
-        Mar 13, 2024
-        nMsWCY6NvgTmGVRzegNSQXtzC2I2
-
-        sequoia.alex.ca@gmail.com
-        Mar 4, 2024
-        Mar 4, 2024
-        4ffL8p73WSgVaWPBX8vnlsoywlI2
-
-        mark.pirvulescu@gmail.com
-        Mar 4, 2024
-        Mar 6, 2024
-        ojgUjs9MIFbRX1V9n6wS3aMYdf03
-*/
 
 function loadEvent() {
     let eventDate = document.getElementById("calendar-div");
