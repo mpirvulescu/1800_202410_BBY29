e2609b0f9873c8874ca1a79f970babc5fed378d1
Mark Pirvulescu <mark.pirvulescu@gmail.com>
2024-04-04 11:02:45 -0700

    Added a footer before log in

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,11 +3,11 @@ function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {     //when "user" is logged in
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_after_login.html'));
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_before_login.html'));
         }
     });
 }
